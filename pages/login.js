22baec0168da4ff68b332805c421528822f1cf68
muhseenat <shuhaib.s@aot-technologies.com>
2022-03-28 11:40:24 +0530

    updated with form validation

--- pages/login.js
+++ pages/login.js
@@ -3,51 +3,28 @@ import { useRouter } from 'next/router';
 import { useForm } from 'react-hook-form';
 import {yupResolver} from '@hookform/resolvers/yup';
 import * as Yup from 'yup';
-import Link from 'next/link'
 import  loginStyles from '../styles/Login.module.css';
-import Input from '../components/AuthInput';
 import axios from '../axios';
 
 const login = () => {
-    const router=useRouter()
-    const [forminput,setFormInput] = useState({});
-     const setInput=(e)=>{
-         const key= e.target.name;
-         const value=e.target.value;
-         setFormInput({...forminput,[key]:value})
-     }
+     // form validation rules 
+     const validationSchema = Yup.object().shape({
+        
+        firstName: Yup.string()
+            .required('First Name is required'),
+       email: Yup.string()
+            .required('Email is required')
+            .email('Email is invalid'),
+    });
 
-    const validationSchema=Yup.object().shape({
-        email:Yup.string()
-           .required('Email is required')
-           .email('Email is invalid'),
-        password:Yup.string()
-             .min(6,'Password must be atleast 6 characters')
-             .required('Password is required')   
-    })
-    // const formOptions = {  };
+    const formOptions = { resolver: yupResolver(validationSchema) };
 
-const {register,handleSubmit,errors}=useForm({
-    resolver: yupResolver(validationSchema)
-});
-// const {errors}=formState;
+    // get functions to build form with useForm() hook
+    const { register, handleSubmit, reset, formState } = useForm(formOptions);
+    const { errors } = formState;
 
- const onSubmit=(data)=>console.log(data);
-//    console.log(forminput);
-//   const onSubmit=async(data)=>{
-//     console.log(data);
-//       e.preventDefault();
-//       try {
-//           console.log(forminput);
-//          const res = await axios.post('/login',forminput);
-//          if(res){
-//             router.push('/')
-//          }
-//       } catch (error) {
-//           console.log(error);
-//       }
-//   }
-
+    const onSubmit = data => console.log(data);
+ 
   return (
     <div>
 <h3 className={loginStyles.heading}>Login </h3>
@@ -56,26 +33,27 @@ const {register,handleSubmit,errors}=useForm({
 
 		<div className={loginStyles.bg}></div>
 
-		<form className={loginStyles.loginform} onSubmit={handleSubmit(onSubmit)}>
+		<form className={loginStyles.loginform}  onSubmit={handleSubmit(onSubmit)}>
 			<header className={loginStyles.header}>
 				<img className={loginStyles.image} src="https://jqlacorte.com/wp-content/uploads/2015/09/jql-job-seekers.png?format=auto&height=80&version=1592223909&width=80"/>
 			</header>
 
-			<div id='inputs' className={loginStyles.inputs}>
-               <Input className={loginStyles.input}  type={"email"} {...register('email')} placeholder={"Email"} name={"email"} setInput={setInput}/>
-               <div className={loginStyles.errors}>{errors?.email?.message}</div>
+            <div className="form-group ">
+                            <label>First Name</label>
+                            <input name="firstName" type="text" {...register('firstName')} className={`form-control ${errors.firstName ? 'is-invalid' : ''}`} />
+                            <div className="invalid-feedback">{errors.firstName?.message}</div>
+            </div>
+            <div className="form-group col">
+                            <label>Email</label>
+                            <input name="email" type="text" {...register('email')} className={`form-control ${errors.email ? 'is-invalid' : ''}`} />
+                            <div className="invalid-feedback">{errors.email?.message}</div>
+             </div>
 
-               <Input className={loginStyles.input}   type={"password"} {...register('password')}  placeholder={"Password"} name={"password"} setInput={setInput}/>
-               {/* <Input className={`loginStyles.input ${errors.email?'loginStyles.invalid':''}`}   type={"password"} {...register('password')}  placeholder={"Password"} name={"password"} setInput={setInput}/> */}
-               <div className={loginStyles.errors}>{errors?.password?.message}</div>
-        
-
-			</div>
-	
-			<button  id='footer' className={loginStyles.button} type="submit">Login</button>
-			<p className={loginStyles.para}> Don't have an account? 
-            <Link href="/signup"><a className={loginStyles.a} >Signup</a></Link></p>
-
+            <div className="form-group">
+                        <button type="submit" className="btn btn-primary mr-1">Register</button>
+                        <button type="button" onClick={() => reset()} className="btn btn-secondary">Reset</button>
+           </div>
+            
 		</form>
 
 
