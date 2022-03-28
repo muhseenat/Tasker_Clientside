f7c52e25e95caa0c54ad54b2739ad28d60451a33
muhseenat <mucktd777@gmail.com>
2022-03-28 21:31:34 +0530

    Post job section

--- components/PostJob.js
+++ components/PostJob.js
@@ -6,14 +6,25 @@ const PostJob = () => {
           <div class="container py-5">
               <div class="row py-5">
                   <div class ="col-lg-7">
-                      <p class="red">WHAT WE ARE DOING</p>
+                      <p class="red pt-5">WHAT WE ARE DOING</p>
                       <h1>24k Talented people are getting Jobs</h1>
-                      <p class="py-4" style="font-weight: 700;">  </p>
-                      <p></p>
+                      <p class="py-4">  njjgfjdgljhlfgkjhklgjhlj</p>
+                      <p>nbnb,vmlkhhkjhgkj;lhgk;ljkhgl;kj</p>
+                      <button className="btn4 mt-3">POST A JOB</button>
+                  </div>
+                  <div className="col-lg-5">
+                      <img src="https://media.istockphoto.com/photos/close-up-of-unrecognizable-person-opening-office-door-picture-id1206393963?b=1&k=20&m=1206393963&s=170667a&w=0&h=jhwV9p1Al0aYw3WEqU0tirBabR-lZoHXT5PF52Y9AWc=" className="img-fluid"/>
                   </div>
               </div>
           </div>
           </section>
+          <style jsx>
+              {
+                  `p{
+                      font-weight:700;
+                  }`
+              }
+          </style>
       </>
   )
 }
